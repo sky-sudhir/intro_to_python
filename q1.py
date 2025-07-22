f5557ca4714c932ac0aa0d41502f9a5d25d6d3c7
Sudhir <sky875025@gmail.com>
2025-07-22 22:30:54 +0530

    q1 complete

---
+++ q1.py
@@ -0,0 +1,7 @@
+name =input("Enter your name")
+age =input("Enter your age")
+city =input("Enter your city")
+hobby =input("Enter your hobby")
+
+
+output="Hello, {}!\nYou are {} years old and live in {}.\nIn your free time, you enjoy {}".format(name, age,city,hobby) 
